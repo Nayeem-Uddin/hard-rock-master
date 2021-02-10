d95d657551b3c2694d2e82976032a3a76873b4d6
Nayeem-uddin <nayeemu02@gmail.com>
2021-02-10 10:57:25 +0600

    first part done

--- hard-rock-master/app.js
+++ hard-rock-master/app.js
@@ -1,4 +1,4 @@
-function searchSongs(){
+function searchSongs() {
     const searchField = document.getElementById('input-field').value;
     //console.log(searchField);
     //searchField.value = '';
@@ -6,5 +6,34 @@ function searchSongs(){
     //console.log(url);
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displaySongs(data.data))
+}
+
+const displaySongs = songs => {
+    const songContainer = document.getElementById('song-container');
+    console.log(songs);
+    songs.forEach(song => {
+
+        const songDiv = document.createElement('div');
+        songDiv.className = "single-result row align-items-center my-3 p-3"
+        songDiv.innerHTML = `
+                <div class="col-md-9">
+                    <h3 class="lyrics-name">${song.title}</h3>
+                    <p class="author lead">Album by <span>${song.album.title}</span></p>
+                    <p class="author lead">Artist Name <span>${song.artist.name}</span></p>
+                    <audio controls>
+                        <source src="${song.preview}" type="audio/ogg">
+                    </audio>
+                </div>
+                <div class="col-md-3 text-md-right text-center">
+                    <button class="btn btn-success">Get Lyrics</button>
+                </div>
+        `;
+        songContainer.appendChild(songDiv);
+
+        //console.log(song);
+        // console.log(song.title);
+        // console.log(song.artist.name);
+        // console.log(song.album.title);
+    });
 }
